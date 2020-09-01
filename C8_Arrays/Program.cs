452446deabbfca25a28592b5f15cde3a64405a6f
Mattias Karlsson <Mattias.E.Karlsson@outlook.com>
2020-09-01 15:02:05 +0200

    C8

---
+++ C8_Arrays/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace C8_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many sibilings do u have?:");
+            var numberofSiblings = Convert.ToInt32(Console.ReadLine());
+
+            if (numberofSiblings < 1)
+            {
+                Console.WriteLine("Okej, inga syskon");
+            }
+            else
+            {
+                string[] siblings = new string[numberofSiblings];
+
+                if (numberofSiblings == 1)
+                {
+                    Console.Write("Vad heter ditt syskon?: ");
+                    siblings[0] = Console.ReadLine();
+                }
+
+                else
+                {
+                    for (int i = 0; i < numberofSiblings; i++)
+                    {
+                        if (i == 0)
+                        {
+                            Console.Write("Whats yours first siblings name?: ");
+                        }
+                        else 
+                        {
+                            Console.Write("And your next siblings name?: ");
+                        }
+                        siblings[i] = Console.ReadLine();
+
+                    }
+
+                }
+                if (numberofSiblings == 1)
+                {
+                    Console.WriteLine($"Ditt syskon heter:");
+                }
+                else
+                {
+                    Console.WriteLine($"Dina syskon heter:");
+                }
+
+                foreach (var sibling  in siblings)
+                {
+                    Console.WriteLine($"{sibling}");
+                }
+
+            }
+            
+        }
+
+    }
+}
