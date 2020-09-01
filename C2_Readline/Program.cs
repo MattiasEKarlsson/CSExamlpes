cc560b361cb0f0fa5347a6432ae9bd694c0da5d6
Mattias Karlsson <Mattias.E.Karlsson@outlook.com>
2020-09-01 11:12:55 +0200

    c2

---
+++ C2_Readline/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace C2_Readline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+
+            Console.Write($"Okej {name}, how old are you?: ");
+            int age =0 ;
+            try
+            {
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input");
+                Console.WriteLine($"Okej {name}, how old are you?: ");
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            finally 
+            {
+                Console.WriteLine($"Okej {name} {age}");
+            }
+
+            Console.WriteLine($"Your name contains {name.Length} charachers.");
+            Console.WriteLine($"{name}, where do you live?");
+            var city = Console.ReadLine();
+
+            Console.WriteLine($"My brother livies in {city}");
+
+
+
+
+
+        }
+    }
+}
