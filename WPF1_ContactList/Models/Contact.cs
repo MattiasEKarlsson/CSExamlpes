880cadc3041ffdc9092e6dbf598f9eab71f3b97f
Mattias Karlsson <Mattias.E.Karlsson@outlook.com>
2020-09-08 17:14:14 +0200

    WPF1

---
+++ WPF1_ContactList/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF1_ContactList.Models
+{
+    class Contact
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Fullname => $"{Firstname} {Lastname}";
+
+
+        public Contact(string firstname, string lastname)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+    }
+}
